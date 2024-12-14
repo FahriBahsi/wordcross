f87fda80505f6c6623abd2a8fd86d98fb0867f54
FahriBahsi <71988973+FahriBahsi@users.noreply.github.com>
2024-12-14 22:39:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import numpy as np
+import os  # Heroku'nun PORT değişkeni için
 
 app = Flask(__name__)
 
@@ -81,4 +82,6 @@ def home():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Heroku'nun PORT değişkenini kullanın
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
